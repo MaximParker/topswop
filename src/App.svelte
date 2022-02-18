c62c0bd8e72ca468ca0875517198a00626bef547
MaximParker <studiocaveat@gmail.com>
2022-02-18 19:25:35 +0000

    Merge with previous work (addUser, removeUser)

--- src/App.svelte
+++ src/App.svelte
@@ -1,77 +1,104 @@
 <script>
-  import { initializeApp } from "firebase/app";
-  import {
-    getFirestore,
-    collection,
-    onSnapshot,
-    doc,
-    deleteDoc,
-  } from "firebase/firestore";
-  import { firebaseConfig } from "./lib/firebaseConfig";
-  import Template from "./lib/components/Template.svelte";
-
-  const init = initializeApp(firebaseConfig);
-  const db = getFirestore();
-
-  const colRef = collection(db, "users");
-
-  let users = [];
-  const userSnapshot = onSnapshot(colRef, (querySnapshot) => {
-      let userArray = []
-    querySnapshot.forEach((user) => {
-      let userData = { ...user.data(), id: user.id};
-      userArray = [userData, ...userArray];
-    });
-    users = userArray
-  });
-
-  const removeUser = async (id) => {
-    await deleteDoc(doc(db, "users", id));
-  };
-
-  export let name;
-</script>
-
-<main>
-  <h1>Hello {name}!</h1>
-  <Template />
-  <ul>
-    {#each users as user}
-      <li><span>{user.forename} {user.surname} ({user.username})</span></li>
-    {/each}
-  </ul>
-  <p>----------------</p>
-  <ul>
-    {#each users as user}
-      <li>
-        <span>{user.forename} {user.surname} ({user.username})</span><button
-          on:click={() => {
-            removeUser(user.id);
-          }}>Delete</button
-        >
-      </li>
-    {/each}
-  </ul>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+	import { initializeApp } from "firebase/app";
+	import {
+	  getFirestore,
+	  collection,
+	  onSnapshot,
+	  addDoc,
+	  doc,
+	  deleteDoc,
+	} from "firebase/firestore";
+	import { firebaseConfig } from "./lib/firebaseConfig";
+	import Template from "./lib/components/Template.svelte";
+  
+	const init = initializeApp(firebaseConfig);
+	export const db = getFirestore();
+  
+	let users = [];
+	
+	const userSnapshot = onSnapshot(collection(db, "users"), (querySnapshot) => {
+		let userArray = []
+	  querySnapshot.forEach((user) => {
+		let userData = { ...user.data(), id: user.id};
+		userArray = [userData, ...userArray];
+	  });
+	  users = userArray
+	});
+  
+	const removeUser = async (id) => {
+	  await deleteDoc(doc(db, "users", id));
+	};
+  
+	export let name;
+  
+	let arrList = [];
+	let newForename = "";
+	let newSurname = "";
+	let newUsername = "";
+  
+	const addUser = async (event) => {
+	  event.preventDefault();
+	  const docRef = await addDoc(collection(db, "users"), {
+		forename: newForename,
+		surname: newSurname,
+		username: newUsername,
+	  });
+	  console.log("Document written with ID: ", docRef.id);
+	};
+  </script>
+  
+  <main>
+	<h1>Hello {name}!</h1>
+	<Template />
+	<form
+	  on:submit={(event) => {
+		addUser(event);
+	  }}
+	>
+	  <input type="text" placeholder="Forename..." bind:value={newForename} />
+	  <input type="text" placeholder="Surname..." bind:value={newSurname} />
+	  <input type="text" placeholder="Username..." bind:value={newUsername} />
+	  <button type="submit">Submit</button>
+	</form>
+  
+	<ul>
+	  {#each users as user}
+		<li><span>{user.forename} {user.surname} ({user.username})</span></li>
+	  {/each}
+	</ul>
+	<p>----------------</p>
+	<ul>
+	  {#each users as user}
+		<li>
+		  <span>{user.forename} {user.surname} ({user.username})</span><button
+			on:click={() => {
+			  removeUser(user.id);
+			}}>Delete</button
+		  >
+		</li>
+	  {/each}
+	</ul>
+  </main>
+  
+  <style>
+	main {
+	  text-align: center;
+	  padding: 1em;
+	  max-width: 240px;
+	  margin: 0 auto;
+	}
+  
+	h1 {
+	  color: #ff3e00;
+	  text-transform: uppercase;
+	  font-size: 4em;
+	  font-weight: 100;
+	}
+  
+	@media (min-width: 640px) {
+	  main {
+		max-width: none;
+	  }
+	}
+  </style>
+  

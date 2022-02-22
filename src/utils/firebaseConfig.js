110ea9677148729cebb2b56d868acf74f14647fe
MaximParker <studiocaveat@gmail.com>
2022-02-22 10:26:59 +0000

    Removed outdated config file

--- src/utils/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA5lDA5DTyzT8mFEhrS9dP-5AQWwD_R374",
-  authDomain: "instaguestbooknc.firebaseapp.com",
-  projectId: "instaguestbooknc",
-  storageBucket: "instaguestbooknc.appspot.com",
-  messagingSenderId: "894155154988",
-  appId: "1:894155154988:web:4a92b077e03c131bb65389",
-};

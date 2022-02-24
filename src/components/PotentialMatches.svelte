a6d9e008ef0f452135f677cd89c851a64a118940
Daniel Everson <danieleverson@Daniels-MacBook-Pro.local>
2022-02-24 16:55:43 +0000

    added queryPotUsers function, queries matches database for user_ids of who like current users items

--- src/components/PotentialMatches.svelte
+++ src/components/PotentialMatches.svelte
@@ -1,5 +1,7 @@
 <script>
       import { user } from "../utils/stores";
+      import { db } from "../utils/api";
+      import { collection, getDocs, query, where } from "firebase/firestore";
 
 let signedIn;
 
@@ -7,11 +9,29 @@ user.subscribe((value) => {
   signedIn = value;
 });
 
+let likingUserId = [];
+
+const query1 = query(collection(db, "matches"), where("item_owner_id","==",`${signedIn.uid}`));
+
+const queryPotUsers = async () => {
+    const querySnapshot = await getDocs(query1);
+    let usersThatLikedMyItem = [];
+    querySnapshot.forEach((doc) => {
+        let userData = { ...doc.data() }
+        usersThatLikedMyItem = [userData.liking_user_id, ...usersThatLikedMyItem];
+        })
+        likingUserId = usersThatLikedMyItem;
+        console.log(likingUserId)
+        }
 
 </script>
 
 <main>
+
   <h2>Potential Matches</h2>
+  <button on:click={() => {
+    queryPotUsers()}}>Click me</button>
+
 </main>
 
 <style>

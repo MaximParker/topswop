f699d9042e86613efdb791a4600415ec3c910f56
MaximParker <studiocaveat@gmail.com>
2022-02-26 15:28:29 +0000

    Remove test login from stores

--- src/utils/stores.js
+++ src/utils/stores.js
@@ -1,2 +1,2 @@
 import { writable } from "svelte/store";
-export const user = writable({uid:"SKTFILPxIPamzZ2EUbNSRgy830q1", email:"test@gmail.com", displayName:"Max"});
+export const user = writable(null);

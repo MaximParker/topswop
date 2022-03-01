1cdbb63e75042a011f10ccde195155eca6b0d673
dweverson <dweverson@gmail.com>
2022-03-01 22:27:18 +0000

    adding back in some imports in api that were removed in merge conflict

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,9 +10,15 @@ import {
   documentId,
   setDoc,
 } from "firebase/firestore";
+import {
+  getStorage,
+  getDownloadURL,
+  uploadBytesResumable,
+  ref as ref_storage,
+} from "firebase/storage";
+
 export const db = getFirestore();
 const storage = getStorage();
-const auth = getAuth();
 
 let newListing = {
   username: "",
@@ -34,7 +40,6 @@ export const postListing = async (event, newListing) => {
 };
 
 export const uploadImage = (event, uid, listingRef) => {
-  const storageRef = ref_storage(storage);
   const metadata = {
     contentType: "image/jpeg",
   };
@@ -45,7 +50,7 @@ export const uploadImage = (event, uid, listingRef) => {
     storage,
     `/files/${uid}/${listingRef}/${file}`
   );
-  const uploadTask = uploadBytesResumable(storageRefImage, file, metadata)
+     uploadBytesResumable(storageRefImage, file, metadata)
     .then((uploadTaskSnapshot) => {
       getDownloadURL(uploadTaskSnapshot.ref).then((downloadURL) => {
         updateListingWithImage(downloadURL, listingRef);

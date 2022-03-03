6dcd8dc20f6707f1a6334c428c14eb055d98a3a9
dweverson <dweverson@gmail.com>
2022-03-03 09:09:20 +0000

    all listings filter function and drop down buttons added. sort by cut and fit

--- src/components/Listings.svelte
+++ src/components/Listings.svelte
@@ -4,6 +4,7 @@
   import ListingCard from "./ListingCard.svelte";
   import { user } from "../utils/stores";
   import { onMount } from "svelte";
+import { each } from "svelte/internal";
 
   let signedIn;
 
@@ -12,6 +13,11 @@
   });
 
   let listingsWithLikes = [];
+
+  let hiddenCut = true;
+  let hiddenFit = true;
+  const cutOptions = ['Unisex','Women','Men','Children','Girls','Boys','Toddlers','Infants'];
+  const fitOption = ['Extra large','Large','Medium','Small','Extra small'];
 
   const getListingsAsync = async () => {
     let listings = [];
@@ -30,6 +36,18 @@
     listings = listingArray;
     return listings;
   };
+
+  const filter = async (fieldName="", option="") => {
+    const allListings = await likeButtonSetting(`${signedIn.uid}`);
+    if (fieldName == ""){
+    listingsWithLikes = allListings
+    } 
+    else {
+    const filteredItems = allListings.filter((item) => item[fieldName] == option)
+    listingsWithLikes = filteredItems
+     }
+     
+  }
 
   const likeButtonSetting = async (current_user) => {
     const listings = await getListingsAsync();
@@ -55,6 +73,8 @@
   onMount(async () => {
     listingsWithLikes = await likeButtonSetting(`${signedIn.uid}`);
   });
+
+
 </script>
 
 <main>
@@ -63,6 +83,38 @@
       <h1 class="text-xl font-bold text-primary align-center text-center">
         All Topswops
       </h1>
+      <div class="dropdown">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label tabindex="0" class="m-1 btn btn-secondary">Filter</label>
+        <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><a on:click={() => {hiddenCut=true, hiddenFit=true, filter() }}>All</a></li>
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><a on:click={() => {hiddenCut=false, hiddenFit=true }}>Cut</a></li>
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><a on:click={() => {hiddenCut=true, hiddenFit=false }}>Fit</a></li>
+        </ul>
+      </div>
+      <div class="dropdown" class:hidden={hiddenFit} id="fit"> 
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label tabindex="0" class="m-1 btn btn-secondary">Fit</label>
+        <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+          {#each fitOption as o}
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><a on:click={() => filter("fit", o)}>{o}</a></li>
+          {/each}
+        </ul> 
+      </div>
+      <div class="dropdown" class:hidden={hiddenCut} id="cut">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label tabindex="0" class="m-1 btn btn-secondary">Cut</label>
+        <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+          {#each cutOptions as o}
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><a on:click={() => filter("cut", o)}>{o}</a></li>
+          {/each}
+        </ul>
+      </div>
     </div>
   </header>
   <ListingCard {listingsWithLikes} />
